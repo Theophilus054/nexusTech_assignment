ffc15a2a646a49b818070bfbd41472dad11a8b26
Theophilus054 <brewjoseph@gmail.com>
2023-08-20 15:15:14 -0700

    initial commit

---
+++ assignment.py
@@ -0,0 +1,17 @@
+# while True:
+#     guess = int(input ("guess a number"))
+#     if i < guess < 15:
+#         print("failure.Try again.")
+#     elif 15 <= guess < 31:
+#         print("ducess! you guessed it.")
+#         break
+#     else:
+#         print("invaild input. please enter a number between 1 and 30")
+# name = input("Enter your name")
+# print(f"hello {name}!")
+
+# age = int(input("enter your age"))
+# if age > 15:
+#     age += 1
+#     print(f"your age: {age}")
+        
